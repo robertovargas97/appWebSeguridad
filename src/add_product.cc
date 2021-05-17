13801ea54e3cf0aca397f499e2bd7d3974f8da51
root <you@example.com>
2021-05-16 18:24:16 -0600

    Probado al menos la parte del get, y se agrego parte del post

--- src/add_product.cc
+++ src/add_product.cc
@@ -24,22 +24,22 @@ int main(int argc, char const *argv[])
 {
 
     Utils file_reader = Utils();
-    //char *header = "/templates/header.html";
+    char *header = "/templates/header.html";
     char *navbar = "/templates/navbar.html";
     char *add_product = "/templates/add_product.html";
     char *footer = "/templates/footer.html";
-    //char *header_content = file_reader.read_file(header, header_content);
+    char *header_content = file_reader.read_file(header, header_content);
     char *navbar_content = file_reader.read_file(navbar, navbar_content);
     char *add_product_content = file_reader.read_file(add_product, add_product_content);
     char *footer_content = file_reader.read_file(footer, footer_content);
 
     printf("Content-type:text/html\r\n\r\n");
-    //printf(header_content);
+    printf(header_content);
     printf(navbar_content);
     printf(add_product_content);
     printf(footer_content);
 
-    //free(header_content);
+    free(header_content);
     free(navbar_content);
     free(add_product_content);
     free(footer_content);
