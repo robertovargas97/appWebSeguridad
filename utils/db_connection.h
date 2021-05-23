2a6c9b482d551623ad45d0a1d2f625b9da3f1dc9
tefii2031 <stephanie.leitonramirez@ucr.ac.cr>
2021-05-22 19:46:15 -0600

    cambios en return

--- utils/db_connection.h
+++ utils/db_connection.h
@@ -30,7 +30,7 @@ public:
     bool add_user(string name, string last_name, string email, string password, string phone_number, string address);
     string get_user_salt(string password);
     bool verify_login(string email, string password, string salt);
-    product get_all_products();
+    vector<product> get_all_products();
     bool exist_in_cart(string email,int code_product);
     vector<product> get_my_cart(string email);
     bool add_in_cart(string email, int code_product);
