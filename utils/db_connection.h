e2db1707f59b94aab384b3ba1abf58d23858c5c6
oigresagetro <check-lab@LAPTOP-33Q2RDMQ.localdomain>
2021-05-23 18:43:08 -0600

    Register user done.

--- utils/db_connection.h
+++ utils/db_connection.h
@@ -26,7 +26,7 @@ public:
     vector<string> get_user_info(string email);
     bool add_comment(string name, string last_name, string email, string comment_type, string comment);
     bool add_product(string name, string price, string description, string category);
-    bool add_user(string name, string last_name, string email, string password, string phone_number, string address);
+    bool add_user(string name, string last_name, string email, string secret, string phone_number, string address, string salt);
     string get_user_salt(string password);
     bool verify_login(string email, string password, string salt);
 
