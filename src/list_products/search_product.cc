600e9649b720b90ec613a92c2d31f9df8a3362b8
root <you@example.com>
2021-06-02 14:07:06 -0600

    Se eliminaron comentarios para poder hacer push a la rama

--- src/list_products/search_product.cc
+++ src/list_products/search_product.cc
@@ -57,9 +57,7 @@ int main(int argc, char const *argv[])
     DBConnection conn = DBConnection();
     DBConnection conn_3 = DBConnection();
     std::map<string, string> form_data = utils.get_post_data();
-    //cout<<"form_data["search_product_form"]";
     lista_productos = conn.search_product(form_data["product_to_search"]);
-    //lista_productos = conn.search_product("agua");
 
     if (lista_productos.size() != 0)
     {
